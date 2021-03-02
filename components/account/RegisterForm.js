696640fd07572c1f8ca479d63c6eb2f3dcf57708
Julian Penagos <julis789@hotmail.com>
2021-03-02 12:04:28 -0500

    Tutorial 64 - React / React-Native - 42 - Validar campos del formulario de registro

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,27 +1,87 @@
+
 import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Button, Icon, Input } from 'react-native-elements'
+import { size } from 'lodash'
+
+import { validateEmail } from '../../utils/helpers'
+
 
 export default function RegisterForm() {
-   const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+    const [errorEmail, setErrorEmail] = useState("")
+    const [errorPassword, setErrorPassword] = useState("")
+    const [errorConfirm, setErrorConfirm] = useState("")
 
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text })
+    
+    }
+
+    const registerUser = () => {
+        if (!validateData()) {
+            return;
+        }
+        console.log(" Fuck yeah!!")
+    }
+
+    const validateData = () => {
+        setErrorConfirm("")
+        setErrorEmail("")
+        setErrorPassword("")
+        let isValid = true
+
+        if (!validateEmail(formData.email)) {
+            setErrorEmail("Debes ingresar un email valido.")
+            isValid = false
+        }
+
+        if(size(formData.password) <6 )
+        {
+            setErrorPassword("Debes ingresar una contraseña de al menos 6 caracteres")
+            isValid = false
+        }
+        if(size(formData.confirm) <6 )
+        {
+            setErrorConfirm("Debes ingresar una confirmación de contraseña de al menos 6 caracteres")
+            isValid = false
+        }
+
+        if(formData.password !== formData.confirm)
+        {
+            setErrorPassword("Las contraseña y la confirmación no coinciden")
+            setErrorConfirm("Las contraseña y la confirmación no coinciden")
+            isValid = false
+        }
+
+        return isValid
+
+    }
     return (
         <View style={styles.form}>
             <Input
                 containerStyle={styles.input}
                 placeholder="Ingresa tu email..."
+                onChange={(e) => onChange(e, "email")}
+                keyboardType="email-address"
+                errorMessage={errorEmail}
+                defaultValue={formData.email}
             />
             <Input
                 containerStyle={styles.input}
                 placeholder="Ingresa tu contraseña..."
                 password={true}
                 secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "password")}
+                errorMessage={errorPassword}
+                defaultValue={formData.password}
                 rightIcon={
                     <Icon
                         type="material-community"
-                        name={ showPassword ? "eye-off-outline" : "eye-outline"}
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
                         iconStyle={styles.icon}
-                        onPress= { () => setShowPassword(!showPassword)}
+                        onPress={() => setShowPassword(!showPassword)}
                     />
                 }
             />
@@ -30,22 +90,29 @@ export default function RegisterForm() {
                 placeholder="Confirma tu contraseña..."
                 password={true}
                 secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "confirm")}
+                errorMessage={errorConfirm}
+                defaultValue={formData.confirm}
                 rightIcon={
                     <Icon
-                    type="material-community"
-                    name={ showPassword ? "eye-off-outline" : "eye-outline"}
-                    iconStyle={styles.icon}
-                    onPress= { () => setShowPassword(!showPassword)}
-                />
+                        type="material-community"
+                        name={showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
                 }
             />
             <Button
                 title="Registrar Nuevo Usuario"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btn}
+                onPress={() => registerUser()}
             />
         </View>
     )
+}
+const defaultFormValues = () => {
+    return { email: "", password: "", confirm: "" }
 }
 
 const styles = StyleSheet.create({
