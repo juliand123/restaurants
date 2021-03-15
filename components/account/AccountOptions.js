a79bcf76538937df583cff9b318671502882991b
Julian Penagos <julis789@hotmail.com>
2021-03-15 16:42:13 -0500

    Tutorial 64 - React / React-Native - 58 - Validaciones para el cambio de contraseña

--- components/account/AccountOptions.js
+++ components/account/AccountOptions.js
@@ -5,6 +5,7 @@ import { map } from 'lodash'
 import Modal from '../Modal'
 import ChangeDisplayNameForm from './ChangeDisplayNameForm'
 import ChangeEmailForm from './ChangeEmailForm'
+import ChangePasswordForm from '../../screens/account/ChangePasswordForm'
 
 export default function AccountOptions({ user, toastRef, setReloadUser }) {
 
@@ -66,7 +67,10 @@ export default function AccountOptions({ user, toastRef, setReloadUser }) {
                 break;
             case "password":
                 setRenderComponent(
-                    <Text>password</Text>
+                    <ChangePasswordForm
+                        setShowModal={setShowModal}
+                        toastRef={toastRef}
+                    />
                 )
                 break;
         }
