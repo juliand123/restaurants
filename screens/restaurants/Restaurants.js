194272384fb9782e3a0e72934cce9f2b0de3b5ca
Julian Penagos <julis789@hotmail.com>
2021-03-15 19:34:53 -0500

    Tutorial 64 - React / React-Native - 60 - Botón flotante para agregar restaurantes

---
+++ screens/restaurants/Restaurants.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+
+
+import { getCurrentUser, isUserLogged } from '../../utils/actions'
+import Loading from '../../components/Loading'
+
+export default function Restaurants() {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+       setUser(getCurrentUser())
+    }, [])
+
+    if (user === null) {
+        return <Loading isVisible={true} text="Cargando..." />
+    }
+
+
+    return (
+        <View style={styles.viewBody}>
+            <Text>Restaurants...</Text>
+            {  
+             user && ( <Icon
+                type="material-community"
+                name="plus"
+                color="#df0024"
+                reverse
+                containerStyle={styles.btnContainer}
+            />)
+           }
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewBody: {
+        flex: 1
+    },
+    btnContainer: {
+        position: "absolute",
+        bottom: 10,
+        right: 10,
+        shadowColor: "black",
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.5
+
+    }
+})
